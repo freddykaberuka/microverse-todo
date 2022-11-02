4ff715883b58bb7185efb256044db9c837f9d7a0
freddykaberuka <freddykaberuka2009@gmail.com>
2022-11-02 08:46:18 +0200

    add linters

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,45 +1,44 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
 import { FaPlusCircle } from 'react-icons/fa';
 
 class InputTodo extends Component {
-    state = {
-     title: ""
-    };
+  state = {
+    title: '',
+  };
 
-    onChange = e => {
-      this.setState({
-      [e.target.name]: e.target.value
-    });
-    };
-
-    handleSubmit = e => {
-        e.preventDefault()
-  if (this.state.title.trim()) {
-    this.props.addTodoProps(this.state.title)
+  onChange = (e) => {
     this.setState({
-      title: "",
-    })
-  } else {
-    alert("Please write item")
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
-};
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
+  };
 
   render() {
     return (
       <form onSubmit={this.handleSubmit} className="form-container">
-        <input type="text" placeholder="Add Todo..." value={this.state.title} onChange={this.onChange} name ="title"/>
-        <button className="input-submit">
-            <FaPlusCircle
-          style={{
-            color: 'darkcyan',
-            fontSize: '20px',
-            marginTop: '2px',
-          }}
-        />
+        <input type="text" placeholder="Add Todo..." value={this.state.title} onChange={this.onChange} name="title" />
+        <button className="input-submit" type="button">
+          <FaPlusCircle
+            style={{
+              color: 'darkcyan',
+              fontSize: '20px',
+              marginTop: '2px',
+            }}
+          />
         </button>
       </form>
-    )
+    );
   }
 }
-export default InputTodo
+export default InputTodo;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import "./App.css"
+import './App.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import TodoContainer from './components/TodoContainer';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <TodoContainer/>
+  <TodoContainer />,
 );
 
 // If you want to start measuring performance in your app, pass a function
